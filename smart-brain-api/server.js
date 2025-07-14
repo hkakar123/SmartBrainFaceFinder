563f596069d6f99fd654bec9578ab9a342a6dbe2
Hikmat Kakar <hkakar2@horizon.csueastbay.edu>
2025-07-13 23:37:50 -0700

    f

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -16,7 +16,7 @@ const db = knex({
   client: 'pg',
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false },
+    ssl: { rejectUnauthorized: true },
   },
   pool: {
     min: 2,
